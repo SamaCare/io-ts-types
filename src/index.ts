5e885d5c85a9757c8f51c6d1f97634534114c77c
Nathan Houle <nathan@nathanhoule.com>
2021-11-08 13:02:31 -0800

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// TODO(ndhoule): Export types from here too
-
 export { BooleanFromString } from "./BooleanFromString";
 export { createDateFromString } from "./DateFromString";
 export { DateFromISOString } from "./DateFromISOString";
@@ -7,3 +5,9 @@ export { Int } from "./Int";
 export { NonEmptyString } from "./NonEmptyString";
 export { Port } from "./Port";
 export { PositiveInt } from "./PositiveInt";
+export {
+  Int as IntT,
+  NonEmptyString as NonEmptyStringT,
+  Port as PortT,
+  PositiveInt as PositiveIntT,
+} from "./types";
